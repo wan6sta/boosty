dd92b10257370498c3c296c6ee74a53a8141fead
wan6sta <wan6sta@gmail.com>
2022-12-01 03:19:43 +0300

    Add stylelint and fix files.

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import { cn } from 'shared/lib/classNames/cn'
-import cls from './LangSwitcher.module.scss'
 import { Button, ButtonMods } from 'shared/ui/Button/Button'
 import { useTranslation } from 'react-i18next'
 
@@ -18,7 +17,7 @@ export const LangSwitcher: FC<LangSwitcherProps> = (props) => {
 
   return (
     <Button
-      className={cn(cls.LangSwitcher, {}, [className])}
+      className={cn('', {}, [className])}
       mods={ButtonMods.LIGHT}
       onClick={changeLanguage}
     >
